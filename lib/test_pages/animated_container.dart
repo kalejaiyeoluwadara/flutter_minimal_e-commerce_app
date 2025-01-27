a3ace8d96c1435a05d65f88ae6902bef83be9ec6
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2025-01-27 23:47:17 +0100

    done with animated container

--- lib/test_pages/animated_container.dart
+++ lib/test_pages/animated_container.dart
@@ -11,6 +11,7 @@ class CustomAnimatedContainer extends StatefulWidget {
 class _CustomAnimatedContainerState extends State<CustomAnimatedContainer> {
   double boxHeight = 100;
   double boxWidth = 100;
+  Color boxColor = Colors.deepPurple;
   void _expandedBox() {
     setState(() {
       boxHeight = 300;
@@ -18,18 +19,24 @@ class _CustomAnimatedContainerState extends State<CustomAnimatedContainer> {
     });
   }
 
+  void _changeColor() {
+    setState(() {
+      boxColor = Colors.blue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: _expandedBox,
+      onTap: _changeColor,
       child: Scaffold(
         backgroundColor: Colors.deepPurple[200],
         body: Center(
           child: AnimatedContainer(
             duration: const Duration(seconds: 1),
-            height: 100,
-            width: 100,
-            color: Colors.deepPurple,
+            height: boxHeight,
+            width: boxWidth,
+            color: boxColor,
           ),
         ),
       ),
